125090cf0f4b3cee43282043149de9de7f7acc6a
danmartin98 <danmartin98@icloud.com>
2022-01-17 16:40:53 +0000

    refactored postgres

--- Project/db/index.js
+++ Project/db/index.js
@@ -1,7 +1,17 @@
-const { Pool } = require("pg");
-const pool = new Pool();
-module.exports = {
-  query: (text, params, callback) => {
-    return pool.query(text, params, callback);
-  },
-};
+import pg from "pg";
+
+const pool = new pg.Pool({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+}) 
+
+export default function query(text, params) {
+  return pool.query(text, params);
+}
+
+export default query;
+
+

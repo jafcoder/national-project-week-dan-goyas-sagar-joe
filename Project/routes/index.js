587cab4c858085dc5bda728f7f754d2f0ffbb274
jafcoder <joefarr113@gmail.com>
2022-01-18 13:47:51 +0000

    get route working with api call

--- Project/routes/index.js
+++ Project/routes/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getContentBySubject } from "../models/index.js";
+import { getContent } from "../models/index.js";
 
 var router = express.Router();
 
@@ -8,10 +8,12 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
 
-router.get("/api/:subject", async function (req, res) {
+router.get("/api/:topic/:subject", async function (req, res) {
   let subject = req.params.subject;
+  let topic = req.params.topic;
   console.log(subject);
-  const data = await getContentBySubject(subject);
+  const data = await getContent(subject, topic);
   res.json({ success: true, payload: data });
 });
+
 export default router;

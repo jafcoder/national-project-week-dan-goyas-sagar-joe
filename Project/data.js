6decd44c5beb4a4463b8d5ecde908c10b9012881
jafcoder <joefarr113@gmail.com>
2022-01-18 10:20:36 +0000

    video data for populate table created

--- Project/data.js
+++ Project/data.js
@@ -14,3 +14,46 @@ const articles = [
     image_link: "https://miro.medium.com/max/1400/0*ms9Vukmueceg78Qg",
   },
 ];
+
+const videos = [
+  {
+    subject: "useState",
+    link: "https://www.youtube.com/watch?v=O6P86uwfdR0",
+    recommended: "bootcamper",
+  },
+  {
+    subject: "useState",
+    link: "https://www.youtube.com/watch?v=mxK8b99iJTg",
+    recommended: "coach",
+  },
+  {
+    subject: "useEffect",
+    link: "https://www.youtube.com/watch?v=0ZJgIjIuY7U",
+    recommended: "bootcamper",
+  },
+  {
+    subject: "useEffect",
+    link: "https://www.youtube.com/watch?v=06Y6aJzTmXY",
+    recommended: "coach",
+  },
+  {
+    subject: "useReducer",
+    link: "https://www.youtube.com/watch?v=cVYp4u1m6iA",
+    recommended: "bootcamper",
+  },
+  {
+    subject: "useReducer",
+    link: "https://www.youtube.com/watch?v=kK_Wqx3RnHk",
+    recommended: "coach",
+  },
+  {
+    subject: "props",
+    link: "https://www.youtube.com/watch?v=IYvD9oBCuJI",
+    recommended: "bootcamper",
+  },
+  {
+    subject: "props",
+    link: "https://www.youtube.com/watch?v=PHaECbrKgs0",
+    recommended: "coach",
+  },
+];

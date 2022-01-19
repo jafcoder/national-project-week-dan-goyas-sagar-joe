2c468f94fced6320362b9ef05576a1010f442839
jafcoder <joefarr113@gmail.com>
2022-01-19 09:34:45 +0000

    css changes to more closely match hi fidelity wireframe, data file changed to have embed links of videos

--- Project/bootcamper-hub/src/Components/Homepage/index.js
+++ Project/bootcamper-hub/src/Components/Homepage/index.js
@@ -1,21 +1,30 @@
+import logo from "../../logo.svg";
+
 export default function Homepage() {
   return (
     <div>
+      <img className="App-logo" src={logo} alt="React-logo" />
       <p className="reactDefinition">
         React is a JavaScript framework that allows developers to create large
-        web applications that can change data, without reloading the page. The
-        main purpose of React is to be fast, scalable, and simple.
+        web applications that can change data, without reloading the page.{" "}
+        <br></br> The main purpose of React is to be fast, scalable, and simple.
       </p>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png"
-        alt="React-logo"
-      />
-      <h3>
-        Link to the <a href="https://beta.reactjs.org/">React Docs</a>
+      <h3 className="react-link">
+        <span>
+          {" "}
+          <a
+            className="react-docs-text"
+            rel="noreferrer"
+            target="_blank"
+            href="https://beta.reactjs.org/"
+          >
+            React Docs
+          </a>
+        </span>
       </h3>
       <p className="ReactDocInfo">
-        The React Documentation is a really helpful and qell written resource,
-        so always make sure to refer to them when needed
+        The React Documentation is a really helpful and well written resource{" "}
+        <br></br>so always make sure to refer to them when needed
       </p>
     </div>
   );
